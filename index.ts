08dd2814c9e64391a7f6b1c7df791a2bf7d865f0
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 14:33:34 +0530

    adding BoxEndStart class

--- index.ts
+++ index.ts
@@ -172,3 +172,26 @@ class BESNode {
         return this 
     } 
 }
+
+class BoxEndStart {
+
+    curr : BESNode = new BESNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
