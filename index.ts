aa921fbf83803ed0298e93fdee2ff817e48bd45d
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 12:59:50 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-const parts : number = 3 
+const parts : number = 4
 const scGap : number = 0.02 / parts 
 const sizeFactor : number = 4.5 
 const colors : Array<string> = [
@@ -19,7 +19,7 @@ class ScaleUtil {
     static maxScale(scale : number, i : number, n : number) : number {
         return Math.max(0, scale - i / n)
     }
-    
+
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
@@ -27,4 +27,31 @@ class ScaleUtil {
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
     } 
+}
+
+class DrawingUtil {
+
+    static drawBoxEndStart(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(sf4 * Math.PI /2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            context.fillRect((w / 2 - size) * sf2, (h / 2 - size) * sf3, size * sf1, size * sf1)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBESNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBoxEndStart(context, scale)
+    }
 }
